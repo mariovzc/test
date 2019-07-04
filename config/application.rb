c336acac7bee1e40cb817ff1df622e008756797c
mariovzc <mario.vizcainos@gmail.com>
2019-07-03 22:21:07 -0500

    fixed error with CORS

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module AyendaTest
   class Application < Rails::Application
     config.load_defaults 5.2
     config.api_only = true
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource '*', :headers => :any, :methods => [:get]
